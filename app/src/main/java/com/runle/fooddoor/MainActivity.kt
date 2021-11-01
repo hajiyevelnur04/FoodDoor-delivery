af899eff817e8b1e5a12d80588b46102448a7392
Elnur <elnur@Elnurs-MacBook-Pro.local>
2021-11-01 14:41:09 +0400

    nav graph was added

--- app/src/main/java/com/runle/fooddoor/MainActivity.kt
+++ app/src/main/java/com/runle/fooddoor/MainActivity.kt
@@ -6,8 +6,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.runle.fooddoor.databinding.ActivityMainBinding
 import com.runle.fooddoor.util.setupWithNavigationController
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private lateinit var navHostFragment: NavHostFragment
+    private var destination = 0
 
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,9 +29,11 @@ class MainActivity : AppCompatActivity() {
 
         setupBottomNavigationBar()
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+    }
+
+    fun showBadge() {
+        /*binding.navView.getOrCreateBadge(R.id.navigation_order).isVisible = true
+        binding.navView.getOrCreateBadge(R.id.navigation_order).number = 1*/
     }
 
     @RequiresApi(Build.VERSION_CODES.S)
@@ -69,9 +73,9 @@ class MainActivity : AppCompatActivity() {
         )
 
         controller.observe(this) { navController ->
-            /*navController.removeOnDestinationChangedListener(addOnDestinationChangedListener)
+            navController.removeOnDestinationChangedListener(addOnDestinationChangedListener)
             navController.addOnDestinationChangedListener(addOnDestinationChangedListener)
-            setupActionBarWithNavController(navController)*/
+            //setupActionBarWithNavController(navController)
         }
 
         navHostFragment =
@@ -80,4 +84,31 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         controller.postValue(navController)
     }
+
+    private val addOnDestinationChangedListener =
+        NavController.OnDestinationChangedListener { _, destination, _ ->
+
+            if (this@MainActivity.destination != destination.id) {
+                this@MainActivity.destination = destination.id
+
+                /*if (destination.id == R.id.orderFragment) {
+                    binding.navView.getBadge(R.id.navigation_order)?.isVisible = false
+                }*/
+
+                when (destination.id) {
+                    R.id.nav_home,
+                    R.id.nav_order,
+                    R.id.nav_fav,
+                    R.id.nav_market,
+                    R.id.nav_profile -> {
+                        //supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                    }
+                    else -> {
+                        //supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                    }
+                }
+            }
+        }
+
+
 }
---
+++ app/src/main/java/com/runle/fooddoor/util/ViewState.kt
@@ -0,0 +1,12 @@
+package com.runle.fooddoor.util
+
+/**
+ * Created by elnur on 01.11.21
+ */
+enum class ViewState {
+    CONTENT,
+    LOADING,
+    ERROR,
+    NETWORK,
+    EMPTY
+}
