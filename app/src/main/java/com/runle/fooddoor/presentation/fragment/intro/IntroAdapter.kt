ecf4948664789fa06b53702f708e58b654e90e4d
Elnur <elnur@Elnurs-MacBook-Pro.local>
2021-11-15 20:17:06 +0400

    intro was added

---
+++ app/src/main/java/com/runle/fooddoor/presentation/fragment/intro/IntroAdapter.kt
@@ -0,0 +1,44 @@
+package com.runle.fooddoor.presentation.fragment.intro
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+import com.runle.fooddoor.R
+import com.runle.fooddoor.databinding.PartialItemIntroBinding
+
+/**
+ * Created by elnur on 15.11.21
+ */
+class IntroAdapter() : PagerAdapter() {
+
+    private val titles = listOf(R.string.title_one,R.string.title_two,R.string.title_three,R.string.title_four)
+    private val description = listOf(R.string.title_one_desc,R.string.title_two_desc,R.string.title_three_desc,R.string.title_four_desc)
+    private val icons = listOf(R.drawable.test,R.drawable.test1,R.drawable.test2,R.drawable.test3)
+
+    var list: List<IntroModel> = listOf(
+        IntroModel(title = titles[0], description = description[0],icon = icons[0]),
+        IntroModel(title = titles[1], description = description[0],icon = icons[1]),
+        IntroModel(title = titles[2], description = description[0],icon = icons[2])
+    )
+
+    override fun instantiateItem(collection: ViewGroup, position: Int): Any {
+        val model = list[position]
+        val binding: PartialItemIntroBinding = PartialItemIntroBinding.inflate( LayoutInflater.from(collection.context), collection, false)
+        binding.model = model
+        collection.addView(binding.root)
+        return binding.root
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object`
+    }
+
+    override fun destroyItem(collection: ViewGroup, position: Int, `object`: Any) {
+        collection.removeView(`object` as View?)
+    }
+    override fun getCount(): Int {
+        return list.size
+    }
+
+}
---
+++ app/src/main/java/com/runle/fooddoor/presentation/fragment/intro/IntroModel.kt
@@ -0,0 +1,3 @@
+package com.runle.fooddoor.presentation.fragment.intro
+
+data class IntroModel(val title: Int?, var description: Int?, val icon: Int?)
